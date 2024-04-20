465373012bfd4827c3e93dec1bb855590f0f5240
serg1971ch <serg1971ch@yandex.ru>
2024-04-20 09:19:18 +0500

    dz.4

--- dz2.4/src/Main.java
+++ dz2.4/src/Main.java
@@ -7,22 +7,24 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            getAuthorization("serg_1971_ch", "3j_georg12_33", "3j_georg12_33");
-        } catch (WrongLoginException | WrongPasswordException e) {
+            getAuthorization("serg_1971_ch?+++", "3j_georg12_33", "3j_georg12_33");
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+        }catch (WrongPasswordException e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("this is all ok!");
+            System.out.println("validation is done!");
         }
     }
 
     private static void getAuthorization(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-        boolean test = login.matches("[[a-z]*\\d\\w]");
-        if (test && login.length() <= 20) {
+        boolean test = login.matches("^[\\w]+$");
+        if (test && login.length() >= 20) {
             throw new WrongLoginException("Логин должен содержать только латинские буквы, " +
                     "цифры и знак подчеркивания и длина символов должен равен или меньше 20 символов");
 
         }
-        if (test && password.length() <= 20) {
+        if (test && password.length() >= 20) {
             throw new WrongPasswordException("Пароль должен содержать только латинские буквы, " +
                     "цифры и знак подчеркивания и длина символов должен равен или меньше 20 символов");
         }
