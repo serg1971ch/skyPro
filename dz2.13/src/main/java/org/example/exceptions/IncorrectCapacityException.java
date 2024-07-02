0128b3a4045cc642bd4e103289f362bcc69d2451
serg1971ch <serg1971ch@yandex.ru>
2024-07-02 12:22:13 +0500

    2.13

---
+++ dz2.13/src/main/java/org/example/exceptions/IncorrectCapacityException.java
@@ -0,0 +1,7 @@
+package org.example.exceptions;
+
+public class IncorrectCapacityException extends RuntimeException{
+    public IncorrectCapacityException(String message) {
+        super(message);
+    }
+}
