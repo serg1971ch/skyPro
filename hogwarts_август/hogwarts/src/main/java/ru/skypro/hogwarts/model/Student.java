fe9e3c022d9ba3d0300a30332df09583c02431dc
Сергей <34056892+serg1971ch@users.noreply.github.com>
2024-08-13 03:52:21 +0300

    .

--- hogwarts_август/hogwarts/src/main/java/ru/skypro/hogwarts/model/Student.java
+++ hogwarts_август/hogwarts/src/main/java/ru/skypro/hogwarts/model/Student.java
@@ -16,26 +16,18 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(name = "id", description = "id студента")
-//    @JsonProperty("id")
     private Long id;
     @Schema(name = "name", description = "Имя студента")
-//    @JsonProperty("name")
     private String name;
     @Schema(name = "age", description = "Возраст студента")
-//    @JsonProperty("age")
     private int age;
 
     @ManyToOne
     @JoinColumn(name = "faculty_id")
-//    @JsonProperty("faculty")
     private Faculty faculty;
 
     public Student( String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-//    public Student(long l, String alex, int i) {
-//    }
-
 }
